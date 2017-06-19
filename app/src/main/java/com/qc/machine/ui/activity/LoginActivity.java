44e4bcdaab2e2b3087d0ad69a3600fc57ad301f4
luyu <540302096@qq.com>
2017-06-19 23:17:04 +0800

    model提交

--- app/src/main/java/com/qc/machine/ui/activity/LoginActivity.java
+++ app/src/main/java/com/qc/machine/ui/activity/LoginActivity.java
@@ -4,11 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.StringCallback;
 import com.qc.machine.R;
 import com.qc.machine.base.BaseActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Response;
+import talex.zsw.baselibrary.util.klog.KLog;
 
 
 public class LoginActivity extends BaseActivity {
@@ -30,5 +35,18 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void initData() {
 
+    }
+
+    private void updateUseState(String sn) {
+        OkGo.get("http://106.15.61.209:7881/plazz/api/qchapi/" + "getShopMachineInfo")
+                .tag(this)
+                .params("token", "1234567890")
+                .params("sn", sn)
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(String string, Call call, Response response) {
+                        KLog.i(string + "  " + response.toString());
+                    }
+                });
     }
 }
